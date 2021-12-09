263d464e52ccdef975775a194ad036a73921b8cb
viraj124 <virajm72@gmail.com>
2021-12-09 09:36:44 +0530

    fix: segment counter update logic in early withdraw mapping

--- src/mapping.ts
+++ src/mapping.ts
@@ -101,7 +101,7 @@ export function handleJoinedGame(event: JoinedGame): void {
 
   if (segmentCounter.length == 0) {
     segmentCounter.push(BigInt.fromI32(1))
-  } else if (player.canRejoin == false) {
+  } else {
     segmentCounter[0] = segmentCounter[0] + BigInt.fromI32(1);
   }
   game.segmentCounter = segmentCounter;
@@ -155,6 +155,7 @@ export function handleEarlyWithdrawal(event: EarlyWithdrawal): void {
   let address = event.params.player
   let gameDropOuts = game.dropOuts
   let player = Player.load(address.toHex())
+  let playerInfo = contract.players(event.params.player)
   gameDropOuts.push(player.id)
   game.dropOuts = gameDropOuts
   game.totalDropouts = BigInt.fromI32(gameDropOuts.length)
@@ -163,6 +164,13 @@ export function handleEarlyWithdrawal(event: EarlyWithdrawal): void {
   gamePlayers.splice(playerIndex, 1);
   game.players = gamePlayers;
   game.totalGamePrincipal = event.params.totalGamePrincipal;
+  let segmentCounter = game.segmentCounter;
+  for (var i = 0; i < segmentCounter.length; i++) {
+    if (i == playerInfo.value4.toI32()) {
+      segmentCounter[i] = segmentCounter[i] - BigInt.fromI32(1);
+    }
+  }
+  game.segmentCounter = segmentCounter;
   game.save();
 
   player.withdrawn = true;
